65db830cd234aa880ae84babcbda32eb4d29919c
Joe Nguwo <nguwojoe@gmail.com>
2026-06-22 18:07:19 +0200

    changed the previous sidebar to a more clean and readable one but not yet done

---
+++ src/lib/components/appSideBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import {Sidebar,SidebarHeader,SidebarContent,SidebarFooter} from "$lib/components/ui/sidebar";
+
+</script>
+<main>
+<Sidebar>
+<SidebarHeader>
+    LAST LEVEL SCHOOL
+</SidebarHeader>
+<SidebarContent>
+NAVIGATION
+
+</SidebarContent>
+<SidebarFooter>
+footer
+</SidebarFooter>
+
+</Sidebar>
+
+</main>
